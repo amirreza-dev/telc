b45dfad603c8aba60b419a1e238cbf72eabb6d4c
Amirreza Kadkhodazadeh <amirreza.dev@protonmail.com>
2024-11-21 15:19:09 +0330

    tailwind init and button added to the main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { PlusIcon } from '@heroicons/react/20/solid';
+
 function App() {
  return (
-  <div>
-   <p>Hello World</p>
+  <div className='mx-auto max-w-7xl text-center'>
+   <button
+    type='button'
+    className='rounded-full bg-indigo-600 p-2 mt-12 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+   >
+    <PlusIcon className='h-5 w-5' aria-hidden='true' />
+   </button>
   </div>
  );
 }
